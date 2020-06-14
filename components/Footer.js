1dee47355cc162ec3090272babed5aa9fd2b2956
unknown <4romgod@gmail.com>
2020-06-14 08:31:44 +0200

    ADDED FOOTER icons

--- components/Footer.js
+++ components/Footer.js
@@ -7,31 +7,66 @@ function Footer() {
             <footer>
                 <div className="footer-one">
                     <div className="row ml-0 mr-0">
-                        <ul className="footer-links col-md-12">
-                            <li>
-                                <Link href={`/privacyPolicy`}>
-                                    <a>Privacy Policy</a>
-                                </Link>
-                            </li>
+                    
+                        <div className="col-md-4 footer-contact-container">
+                            <div style={{display: 'flex'}}>
+                                <img
+                                    className="footer-icon"
+                                    src="/images/blog.png"
+                                    alt="azblogs logo"
+                                />
+                                <p className="pl-2"><a>AZBLOGS</a></p>
+                            </div>
 
-                            <li>
-                                <Link href={`/newsletter`}>
-                                    <a>Newsletter</a>
-                                </Link>
-                            </li>
+                            <div style={{display: 'flex'}}>
+                                <img
+                                    className="footer-icon"
+                                    src="/images/email.png"
+                                    alt="email icon"
+                                />
+                                <p className="pl-2"><a>azblogs365@gmail.com</a></p>
+                            </div>
+                            <div style={{display: 'flex'}}>
+                                <img
+                                    className="footer-icon"
+                                    src="/images/saflag.jpg"
+                                    alt="phone icon"
+                                />
+                                <p className="pl-2"><a>+27(0) 63 289 9004</a></p>
+                            </div>
+                        </div>
 
-                            <li>
-                                <Link href={`/contact`}>
-                                    <a>Contact</a>
-                                </Link>
-                            </li>
+                        <div className="col-md-4">
+                            <ul className="footer-links">
+                                <li>
+                                    <Link href={`/privacyPolicy`}>
+                                        <a>Privacy Policy</a>
+                                    </Link>
+                                </li>
 
-                            <li>
-                                <Link href={`/`}>
-                                    <a>Hire Me</a>
-                                </Link>
-                            </li>
-                        </ul>
+                                <li>
+                                    <Link href={`/newsletter`}>
+                                        <a>Newsletter</a>
+                                    </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={`/contact`}>
+                                        <a>Contact</a>
+                                    </Link>
+                                </li>
+
+                                <li>
+                                    <Link href={`/`}>
+                                        <a>Hire Me</a>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="col-md-4">
+                            
+                        </div>
                     </div>
                 </div>
 
