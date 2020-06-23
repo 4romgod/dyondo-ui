7f85db2bbeccaa70df2e5f7e9b3dd51fc19cc038
unknown <4romgod@gmail.com>
2020-06-24 00:08:58 +0200

    Used cdn for bootstrap

--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,7 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" type="image/png" href="/images/logo.png" />
 
-          <link rel="stylesheet" href="/bootstrap/bootstrap.min.css" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
 
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="/css/styles.css" />
@@ -46,9 +46,9 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
 
-          <script src="/bootstrap/jquery-3.2.1.slim.min.js"></script>
-          <script src="/bootstrap/bootstrap.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
           <script src="/bootstrap/popper.min.js"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
         </body>
 
       </Html>
