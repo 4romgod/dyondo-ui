87af814fd1b3a46cdf4d156e75694b94896d6a4f
unknown <4romgod@gmail.com>
2020-06-23 23:22:33 +0200

    Changed favicon to logo

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ class MyDocument extends Document {
         <Head >
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="icon" type="image/png" href="/images/favicon.ico" />
+          <link rel="icon" type="image/png" href="/images/logo.png" />
 
           <link rel="stylesheet" href="/bootstrap/bootstrap.min.css" />
 
